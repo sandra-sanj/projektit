0c827d7063c3308ab07bdaec87ce00c09ee57b3f
sandra-sanj <sandra.juursoo@metropolia.fi>
2024-11-05 14:16:32 +0200

    changes

--- mod12/2_weather.py
+++ mod12/2_weather.py
@@ -3,21 +3,24 @@ import json
 
 
 api_key = False
-kelvin = 273.15
+units = 'metric'
+language = 'FI'
 
-city_name = input('Enter city name: ')
+city_name = "york" #input('Enter city name: ')
 
 try:
-    request = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}'
+    request = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units={units}&lang={language}'
     result = requests.get(request)
 
     if result.status_code == 200:
         json_result = result.json()
 
+        # print json result with indents
+        #print(json.dumps(json_result, indent=2))
+
         temperature = json_result['main']['temp']
-        temperature_celcius = temperature - kelvin
         weather_description = json_result['weather'][0]['description']
-        print(f'{weather_description.capitalize()}, {round(temperature_celcius, 1)}°C')
+        print(f'{weather_description.capitalize()}, {round(temperature, 1)}°C')
 
 except requests.exceptions.RequestException as e:
     print(e)
